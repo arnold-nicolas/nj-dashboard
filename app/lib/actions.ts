1420b56862cd0733077b12c6961f2cc9d5f2017f
arnold-nicolas <bugoy2143@yahoo.com>
2026-03-07 18:13:41 -0800

    Create a server action, insert to DB. Revalidate and redirect

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,13 +1,36 @@
 'use server';
 
+import { z } from 'zod';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import postgres from 'postgres';
+
+const sql = postgres(process.env.POSTGRESS_URL!, { ssl: 'require' });
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({id: true, date:true});
+
 export async function createInvoice(formData: FormData) {
-    console.log('test');
-    const rawFormData = {
+    const { customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
-    };
+    });
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
 
-    console.log(rawFormData);
+    await sql`
+        insert into invoices(customer_id, amount, status, date)
+        values (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
 }
-
